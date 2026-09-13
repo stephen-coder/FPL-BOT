500a23ca36b7dc0e3cd7f2138d5d01f866d0dfc2
stephen-coder <63968962+stephen-coder@users.noreply.github.com>
2026-09-13 14:25:18 +0300

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,4 @@
-from bot import app, start_telegram_bot
-import threading
-
-# Start the Telegram bot in a background thread when Gunicorn boots the app
-bot_thread = threading.Thread(target=start_telegram_bot)
-bot_thread.daemon = True
-bot_thread.start()
+from bot import app
 
 if __name__ == "__main__":
     app.run()
