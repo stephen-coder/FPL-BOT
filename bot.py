cbc7b6345bf7f120322f97714d08dd38f55751d8
stephen-coder <63968962+stephen-coder@users.noreply.github.com>
2026-09-16 18:39:25 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1531,7 +1531,7 @@ if not TOKEN:
     logger.warning("TELEGRAM_BOT_TOKEN is not set.")
 
 bot = Bot(token=TOKEN if TOKEN else "000000:INVALID")
-application = Application.builder().bot(bot).updater(None).build()
+update = Update.de_json(json_data, application.bot))
 bot_instance = FPLBot()
 
 application.add_handler(CommandHandler("start", bot_instance.start))
